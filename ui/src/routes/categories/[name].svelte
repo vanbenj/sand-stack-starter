835b92b748141770010f48bae0d1b9a6c6d3959b
Benjamin Fayle <benjamin.fayle@tagga.com>
2019-12-14 04:33:24 -0800

    Removed bad query call

--- ui/src/routes/categories/[name].svelte
+++ ui/src/routes/categories/[name].svelte
@@ -14,9 +14,6 @@
 
   export async function preload({ params }) {
     return {
-      cache: await client.query({
-        query: GET_BUSINESSES
-      }),
       category: params.name
     };
   }
