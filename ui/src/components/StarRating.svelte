a1a814f7cf1af31347aec5c04e3a3bc99762c076
Benjamin Fayle <benjamin.fayle@tagga.com>
2019-12-17 03:51:38 -0800

    Changed css variable name

--- ui/src/components/StarRating.svelte
+++ ui/src/components/StarRating.svelte
@@ -5,17 +5,17 @@
 
 <style>
 h1 {
-  --columns:50%;
+  --ratingPercent:50%;
   display: inline-block;
   margin: 0;
   line-height: 1em;
   font-family: helvetica, arial, sans-serif;
   font-weight: bold;
   font-size: 15px;
-  background: linear-gradient(to right, gold var(--columns),lightgrey var(--columns));  
+  background: linear-gradient(to right, gold var(--ratingPercent),lightgrey var(--ratingPercent));  
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }
 </style>
 
-<h1 style="--columns:{ratingPercent}">★★★★★</h1>
+<h1 style="--ratingPercent:{ratingPercent}">★★★★★</h1>
